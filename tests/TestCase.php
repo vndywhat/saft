2473ee7f490ddc9ef4ede7a71990908d6d061e41
vndywhat <mau5d@yandex.ru>
2022-03-14 21:09:43 +0600

    update test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,14 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseMigrations;
 
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
